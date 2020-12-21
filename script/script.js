1014785da368ac1ed9a27e58aeed9b69d63b8e6f
цареградский илья <Koriolan2@gmail.com>
2020-12-21 17:54:12 +0200

    Home work for Lesson20

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     'use strict';
-
+    // Таймер
     function countTimer(deadline) {
         let timerDays = document.querySelector('#timer-days'),
             timerHours = document.querySelector('#timer-hours'),
@@ -52,14 +52,24 @@ window.addEventListener('DOMContentLoaded', () => {
         const btnMenu = document.querySelector('.menu'),
             menu = document.querySelector('menu'),
             closeBtn = document.querySelector('.close-btn'),
-            menuItems = menu.querySelectorAll('ul>li');
-        const handlerMenu = () => {
+            menuItems = menu.querySelectorAll('ul>li'),
+            body = document.body;
 
+        body.addEventListener('click', (event) => {
+            let target = event.target;
+            
+            if (target.closest('.menu') || target.closest('.close-btn')) {
+                handlerMenu();
+            }
+        });
+
+        const handlerMenu = () => {
             menu.classList.toggle('active-menu');
         };
-        btnMenu.addEventListener('click', handlerMenu);
 
-        closeBtn.addEventListener('click', handlerMenu);
+        // btnMenu.addEventListener('click', handlerMenu);
+
+        // closeBtn.addEventListener('click', handlerMenu);
 
         menuItems.forEach((elem => {
             elem.addEventListener('click', handlerMenu);
@@ -94,7 +104,6 @@ window.addEventListener('DOMContentLoaded', () => {
                
         });
     }
-
     togglePopUp();
 
     // табы
@@ -129,6 +138,5 @@ window.addEventListener('DOMContentLoaded', () => {
 
         });
     };
-
     tabs();
 });
