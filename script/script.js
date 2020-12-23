804bb0cc3a9b0cf5e46d743fa238b7676cf195e3
Kozmin Jurii <Koriolan2@gmail.com>
2020-12-23 22:37:32 +0200

    Home work for Lesson23

--- script/script.js
+++ script/script.js
@@ -233,4 +233,35 @@ window.addEventListener('DOMContentLoaded', () => {
     };
     slider();
     
+    // hover-эффект на фото
+    const myHover = () => {
+        const wrapper = document.querySelector('.command');
+        
+        wrapper.addEventListener('mouseover', (even) => {
+            let target = even.target;
+            if(target.classList.contains('command__photo')) {
+              let c = target.src;
+              target.src = target.dataset.img;
+                        
+              target.addEventListener('mouseout', (even) => {
+                target.src = c;
+              });
+            }
+
+        });
+
+    };
+    myHover();
+
+    //валидация полей формы калькулятора
+    const validation = () =>{
+        let fields = document.querySelectorAll('.calc-square, .calc-count, .calc-day');
+
+        fields.forEach((elem) => {
+            elem.addEventListener('input', () => {
+                elem.value = elem.value.replace(/\D/g,'');
+            });
+        });
+    };
+    validation()
 });
